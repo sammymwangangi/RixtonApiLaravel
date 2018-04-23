a973f0de606b1709e8145f738bdbed094095133e
sammymwangangi <sammymwinzi2@gmail.com>
2018-04-23 17:52:05 +0300

    added curl functionality

--- config/app.php
+++ config/app.php
@@ -151,6 +151,7 @@ return [
          * Package Service Providers...
          */
         Asvae\ApiTester\ServiceProvider::class,
+        Ixudra\Curl\CurlServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -208,6 +209,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Curl' => Ixudra\Curl\Facades\Curl::class,
 
     ],
 
